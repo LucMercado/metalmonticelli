1ac8ec11b1f469c054df8cbc43dded3ae9562df9
Lucas Mercado <lucasmmercado99@gmail.com>
2024-07-29 14:38:26 -0300

    Slider productos

---
+++ script/slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          }
+      ]
+  });
+});
